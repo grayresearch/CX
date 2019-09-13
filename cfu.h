acc896137ac8c657d200ab892cad978d3152994a
Jan Gray <jan@fpga.org>
2019-09-13 12:20:39 -0700

    Strip tabs

--- cfu.h
+++ cfu.h
@@ -4,13 +4,13 @@
 `define vloff_width /* verilator lint_off WIDTH */
 `define vlon_width /* verilator lint_on WIDTH */
 
-`define CFU_INTERFACE_ID	[CFU_INTERFACE_ID_W-1:0]
-`define CFU_FUNCTION_ID		[CFU_FUNCTION_ID_W-1:0]
-`define CFU_REORDER_ID		[CFU_REORDER_ID_W-1:0]
-`define CFU_REQ_RESP_ID		[CFU_REQ_RESP_ID_W-1:0]
-`define CFU_REQ_DATA		[CFU_REQ_DATA_W-1:0]
-`define CFU_RESP_DATA		[CFU_RESP_DATA_W-1:0]
-`define CFU_ERROR_ID		[CFU_ERROR_ID_W-1:0]
+`define CFU_INTERFACE_ID    [CFU_INTERFACE_ID_W-1:0]
+`define CFU_FUNCTION_ID     [CFU_FUNCTION_ID_W-1:0]
+`define CFU_REORDER_ID      [CFU_REORDER_ID_W-1:0]
+`define CFU_REQ_RESP_ID     [CFU_REQ_RESP_ID_W-1:0]
+`define CFU_REQ_DATA        [CFU_REQ_DATA_W-1:0]
+`define CFU_RESP_DATA       [CFU_RESP_DATA_W-1:0]
+`define CFU_ERROR_ID        [CFU_ERROR_ID_W-1:0]
 
 `define CFU_L0_PARAMETERS(INPUTS,WIDTH) \
     parameter CFU_FUNCTION_ID_W = 16, \
